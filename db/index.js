f6a9428db2326bd5a461a572c2bda18f99f279f8
Steven Vitali <svitali@seas.upenn.edu>
2015-11-22 03:19:31 -0500

    Add detailed vogels schemas for user, status, comment tables

--- db/index.js
+++ db/index.js
@@ -6,21 +6,62 @@ vogels.AWS.config.loadFromPath('../credentials.json');
 
 var User = vogels.define('User', {
   hashKey: 'email',
-  timestamps: true,
   schema: {
     _id: vogels.types.uuid(),
     email: Joi.string().email,
+    passwordHash: Joi.string(),
     firstName: Joi.string(),
     lastName: Joi.string(),
     birthdate: Joi.date(),
     school: Joi.string(),
     work: Joi.string(),
     interests: vogels.types.stringSet(),
-    // A set of emails, presumably
-    friends: vogels.types.stringSet()
+    createdAt: Joi.date()
+  },
+  indexes: [
+    // Example of a global index (different hashKey)
+    { hashKey: 'school',
+      rangeKey: 'lastName',
+      name: 'SchoolIndex',
+      type: 'global' 
+    }
+  ]
+});
+
+var Status = vogels.define('Status', {
+  hashKey: 'posterEmail',
+  rangeKey: 'statusId',
+  schema: {
+    posterEmail: Joi.string(),
+    statusId: Joi.number(),
+    datePosted: Joi.date(),
+    content: Joi.string(),
+    likes: vogels.types.stringSet() // of liker emails
+  },
+  indexes: [
+    // Example of a local index (different hashKey)
+    { hashKey: 'posterEmail',
+      rangeKey: 'datePosted',
+      name: 'DatePostedIndex',
+      type: 'local' 
+    }
+  ]
+});
+
+var Comment = vogels.define('Comment', {
+  hashKey: 'statusId', // = "Status.posterEmail + Status.statusId"
+  rangeKey: 'datePosted',
+  schema: {
+    statusId: Joi.string(),
+    content: String,
+    likes: vogels.types.stringSet(), // liker emails
+    datePosted: Joi.date(),
+    commenter: Joi.string() // email
   }
 });
 
-return {
-  User: new Model(User)
+module.exports = {
+  User: User,
+  Status: Status,
+  Comment: Comment
 };
