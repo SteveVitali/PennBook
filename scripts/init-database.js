f969b5398a2056d0b870b780b71916c9da65de9d
Steven Vitali <svitali@seas.upenn.edu>
2015-12-07 20:15:00 -0500

    Add database initialization script

---
+++ scripts/init-database.js
@@ -0,0 +1,19 @@
+// This script initializes all the databases in DynamoDB
+var _ = require('lodash');
+var vogels = require('vogels');
+var async = require('async');
+var models = require('../db');
+
+var tableMetadata = {};
+
+_.each(models, function(model, name) {
+  tableMetadata[model.tableName] = {
+    readCapacity: 1,
+    writeCapacity: 1
+  };
+});
+
+vogels.createTables(tableMetadata, function(err) {
+  err && console.log(err);
+  !err && console.log('Successfully created all databases');
+});
