4c0ced02d8dcd9017f68c8fb780628e71f737b9f
Steven Vitali <svitali@seas.upenn.edu>
2015-12-07 23:40:27 -0500

    Add navigation bar and news feed page template components

---
+++ public/views/navigation-bar.jsx
@@ -0,0 +1,81 @@
+var $ = require('jquery');
+var _ = require('lodash');
+var React = require('react');
+var ReactBootstrap = require('react-bootstrap');
+
+var NavigationBarView = React.createClass({
+  propTypes: {
+    app: React.PropTypes.object.isRequired
+  },
+
+  getDefaultProps() {
+    return {
+      app: null
+    };
+  },
+
+  getInitialState() {
+    return {
+      searchTerm: ''
+    };
+  },
+
+  search(searchTerm) {
+    console.log('Searching for', searchTerm);
+  },
+
+  render() {
+    var Nav = ReactBootstrap.Nav;
+    var Navbar = ReactBootstrap.Navbar;
+    var NavItem = ReactBootstrap.NavItem;
+    var NavBrand = ReactBootstrap.NavBrand;
+    var Input = ReactBootstrap.Input;
+    var NavDropdown = ReactBootstrap.NavDropdown;
+    var MenuItem = ReactBootstrap.MenuItem;
+
+    return (
+      <nav className='navbar navbar-inverse navbar-static-top'>
+        <div className='container'>
+          <div className='navbar-header'>
+            <button type='button' className='navbar-toggle collapsed'
+              data-toggle='collapse'
+              data-target='#navbar'
+              aria-expanded='false'
+              aria-controls='navbar'>
+              <span className='sr-only'>Toggle navigation</span>
+              <span className='icon-bar'></span>
+              <span className='icon-bar'></span>
+              <span className='icon-bar'></span>
+            </button>
+            <a className='navbar-brand' href='#'>Project name</a>
+          </div>
+          <div id='navbar' className='navbar-collapse collapse'>
+            <form className='navbar-form navbar-left'>
+              <div className='form-group'>
+                <Input type='text' placeholder='Search'
+                  onChange={(e) => {
+                    this.setState({ searchTerm: e.target.value });
+                  }}
+                  onKeyPress={(e) => {
+                    if (event.keyCode === 13) {
+                      this.search(this.state.searchTerm);
+                    }
+                }}/>
+              </div>
+            </form>
+            <ul className='nav navbar-nav navbar-right'>
+              <NavDropdown title='Settings'>
+                <MenuItem href='/#profile'>View Profile</MenuItem>
+                <MenuItem href='/#profile/edit'>Edit Profile</MenuItem>
+                <MenuItem divider />
+                <MenuItem href='/#logout'>Log Out</MenuItem>
+              </NavDropdown>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+});
+
+module.exports = NavigationBarView;
---
+++ public/views/news-feed.jsx
@@ -0,0 +1,63 @@
+var $ = require('jquery');
+var _ = require('lodash');
+var React = require('react');
+var ReactBootstrap = require('react-bootstrap');
+var NavigationBarView = require('./navigation-bar.jsx');
+
+var NewsFeedView = React.createClass({
+  propTypes: {
+    app: React.PropTypes.object.isRequired
+  },
+
+  getDefaultProps() {
+    return {
+      app: null
+    };
+  },
+
+  getInitialState() {
+    return {
+      status: ''
+    };
+  },
+
+  postStatus() {
+    console.log('Posting status', this.state.status);
+  },
+
+  render() {
+    var Row = ReactBootstrap.Row;
+    var Col = ReactBootstrap.Col;
+    var Panel = ReactBootstrap.Panel;
+    var Button = ReactBootstrap.Button;
+    var Input = ReactBootstrap.Input;
+    return (
+      <span>
+        <NavigationBarView/>
+        <div className='container'>
+        <Row>
+          <Col md={2}>
+            <a href='/#profile'>View Profile</a>
+            <br/>
+            <a href='/#profile/edit'>Edit Profile</a>
+          </Col>
+          <Col md={8}>
+            <Panel header='Updated Status'>
+              <Input type='textarea' placeholder="What's on your mind?"
+                onChange={(e) => {
+                  this.setState({ status: e.target.value });
+              }}/>
+              <Button onClick={this.postStatus}>Post</Button>
+            </Panel>
+          </Col>
+          <Col md={2}>
+            Online Friends
+          </Col>
+        </Row>
+        </div>
+      </span>
+    );
+  }
+});
+
+module.exports = NewsFeedView;
