57f1bf526b69542fd961bcf6aacef0a113a29bab
Steven Vitali <svitali@seas.upenn.edu>
2015-12-16 01:16:18 -0500

    Lazy load status users and render status like FB does

--- public/views/status.jsx
+++ public/views/status.jsx
@@ -1,5 +1,8 @@
+var async = require('async');
+var moment = require('moment');
 var React = require('react');
 var ReactBootstrap = require('react-bootstrap');
+var Loader = require('react-loader');
 
 var StatusView = React.createClass({
   propTypes: {
@@ -13,12 +16,68 @@ var StatusView = React.createClass({
   },
 
   getInitialState() {
-    return {};
+    var posterId = this.props.status.posterId;
+    var recipientId = this.props.status.recipientId;
+    return {
+      poster: this.props.appStore.get(posterId, 'Users'),
+      recipient: this.props.appStore.get(recipientId, 'Users')
+    };
+  },
+
+  lazyLoadUsers() {
+    if (this.state.poster && this.state.recipient) {
+      console.log('users are loaded for status');
+      return true;
+    }
+    var posterId = this.props.status.posterId;
+    var recipientId = this.props.status.recipientId;
+    var appStore = this.props.appStore;
+
+    appStore.fetch([posterId, recipientId], 'Users', () => {
+      console.log('lazily loaded users related to status');
+      this.setState({
+        poster: appStore.get(posterId, 'Users'),
+        recipient: appStore.get(recipientId, 'Users')
+      });
+    });
   },
 
   render() {
+    var state = this.state;
+    var props = this.props;
+
+    var postedDate = new Date(props.status.datePosted);
+    var timeSince = moment(postedDate).format('MMMM Do YYYY, h:mm:ss a');
+
+    var extractName = (user) => {
+      return user && [user.firstName, user.lastName].join(' ');
+    };
+
+    var recipientName = extractName(state.recipient);
+    var posterName = extractName(state.poster);
+
+    var recipientUrl = '/#profiles/id/' + this.props.status.recipientId;
+    var posterUrl = '/#profile/id/' + this.props.status.posterId;
+
     return (
-      <span>{this.props.status.content}</span>
+      <span>
+        <Loader loaded={this.lazyLoadUsers()} scale={0.8}>
+          <p>
+            <strong>
+              <a href={posterUrl}>
+                {posterName}
+              </a>
+              &#x25BA;
+              <a href={recipientUrl}>
+                {recipientName}
+              </a>
+            </strong>
+            <br/>
+            {timeSince}
+          </p>
+          {this.props.status.content}
+        </Loader>
+      </span>
     );
   }
 });
